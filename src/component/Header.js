81339b468c8e0d67d4af561b7342ab219ea91db1
premchand <choudharypremchand16@gmail.com>
2022-06-26 17:48:27 +0530

    amazon

--- src/component/Header.js
+++ src/component/Header.js
@@ -14,7 +14,7 @@ function Header({ setInputState }) {
   return (
     <div className="header">
     <Link to="/">
-      <img className="image" src="/Images/logo.png" alt="" />
+      <img className="image" src="./images/logo.png" alt="logo" />
       </Link>
       <div className="search-bttn">
         <input type="text" placeholder="Search Your Product"  onChange={manageInput} />
@@ -32,6 +32,7 @@ function Header({ setInputState }) {
       <Link to="/login">
       <button className="login">Login</button>
       </Link>
+     
     </div>
   );
 }
--- src/component/Login.js
+++ src/component/Login.js
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import htmling from '../logo.png'
 export default function Login() {
   return (
+    <>
     <div>this is login in button</div>
+    <img src={htmling} alt=''/>
+    </>
   )
 }
 
